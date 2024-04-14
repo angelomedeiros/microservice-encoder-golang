97a8cba59d22b5e15d692a25e69ff3043e2e81a0
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-04-14 16:32:42 -0300

    feat: criando entidade video e test

---
+++ encoder/domain/video_test.go
@@ -0,0 +1,41 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIsNotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV1().String()
+	video.ResourceID = "b"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Nil(t, err)
+}
